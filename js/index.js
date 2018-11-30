9dce2ebc2e76565febb3a055f0f3933d9c76f63b
nataliag3 <44440489+nataliag3@users.noreply.github.com>
2018-11-30 17:06:51 +0000

    changing paths of mei

--- js/index.js
+++ js/index.js
@@ -20,4 +20,4 @@ options = {
 vrvToolkit.setOptions(options)
 
 /* Show the score */
-showScore('score', 'ORIGINAL Post_malone_better_now.mei') // <-- CHANGE THIS TO YOUR FILE
+showScore('score', 'dataset\WEEK 3\ORIGINAL Post_malone_better_now.mei') // <-- CHANGE THIS TO YOUR FILE
